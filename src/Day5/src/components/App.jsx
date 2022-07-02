b2c57ee7ddf47c8b2849cdb6dfd08e7d0118505b
Abhimanyu Ajudiya <88022199+AbhimanyuAjudiya@users.noreply.github.com>
2022-07-02 18:57:48 +0530

    Add files via upload

---
+++ src/Day5/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Contact from "../contacts";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
