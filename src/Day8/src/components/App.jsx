d33748a0d0fe42a43a1ced908beefaddb1292989
Abhimanyu Ajudiya <88022199+AbhimanyuAjudiya@users.noreply.github.com>
2022-07-05 16:15:12 +0530

    Add files via upload

---
+++ src/Day8/src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Entry from "./Entry"
+import Emojipedia from "../emojipedia";
+
+function createEmoji(content){
+  return (
+    <Entry
+      key={content.id}
+      id={content.id}
+      emoji={content.emoji}
+      name={content.name}
+      meaning={content.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      {Emojipedia.map(createEmoji)}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Day8/src/components/Entry.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Entry(prop) {
+  return (
+    <dl className="dictionary">
+      <div className="term">
+        <dt>
+          <span className="emoji" role="img" aria-label="Tense Biceps">
+            {prop.emoji}
+          </span>
+          <span>{prop.name}</span>
+        </dt>
+        <dd>{prop.meaning}</dd>
+      </div>
+    </dl>
+  );
+}
+
+export default Entry;
